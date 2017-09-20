732995baf02ea98bf21a6e5dabf075e2f8960443
tarunsingh070 <31713053+tarunsingh070@users.noreply.github.com>
2017-09-20 19:31:15 -0400

    Initial Commit

---
+++ Intermediate-User-Interactions/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Intermediate-User-Interactions
+//
+//  Created by Tarun Singh on 2017-09-20.
+//  Copyright © 2017 Tarun Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
