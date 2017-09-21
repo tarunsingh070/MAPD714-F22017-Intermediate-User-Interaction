fd8552b3f8bd8aad43de1fff5226256d4d0bc4d0
tarunsingh070 <31713053+tarunsingh070@users.noreply.github.com>
2017-09-20 22:32:23 -0400

    Added partial functionally.

--- Intermediate-User-Interactions/ViewController.swift
+++ Intermediate-User-Interactions/ViewController.swift
@@ -3,7 +3,7 @@
  * Date: September 20, 2017
  * Student ID: 300967393
  * Description: Intermediate User Interaction
- * Version: 0.2 - Added Outlets and Actions
+ * Version: 0.3 - Added Partial Functionality
  */
 
 import UIKit
@@ -14,6 +14,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var numberTextField: UITextField!
     @IBOutlet weak var sliderLabel: UILabel!
+    @IBOutlet weak var leftSwitch: UISwitch!
+    @IBOutlet weak var rightSwitch: UISwitch!
+    @IBOutlet weak var doSomethingButton: UIButton!
     
     // INHERITED METHODS ++++++++++++++++++++++++++++
     override func viewDidLoad() {
@@ -22,16 +25,38 @@ class ViewController: UIViewController {
     }
     
     // ACTIONS METHODS ++++++++++++++++++++++++++++
-    @IBAction func onSliderChanged(_ sender: Any) {
+    @IBAction func onSliderChanged(_ sender: UISlider) {
+        sliderLabel.text = String(lroundf(sender.value))
     }
     
-    @IBAction func onSegmentedControlChanged(_ sender: Any) {
+    @IBAction func onSegmentedControlChanged(_ sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == 0 {
+            leftSwitch.isHidden = false
+            rightSwitch.isHidden = false
+            doSomethingButton.isHidden = true
+        } else {
+            leftSwitch.isHidden = true
+            rightSwitch.isHidden = true
+            doSomethingButton.isHidden = false
+        }
     }
     
     @IBAction func onSomethingPressed(_ sender: Any) {
     }
     
     @IBAction func onSwitchChanged(_ sender: UISwitch) {
+        let setting = sender.isOn
+        leftSwitch.setOn(setting, animated: true)
+        rightSwitch.setOn(setting, animated: true)
+    }
+    
+    
+    @IBAction func textFieldDoneEditing(_ sender: UITextField) {
+    }
+    
+    @IBAction func onTapGestureRecognized(_ sender: UITapGestureRecognizer) {
+        nameTextField.resignFirstResponder()
+        numberTextField.resignFirstResponder()
     }
 }
 
